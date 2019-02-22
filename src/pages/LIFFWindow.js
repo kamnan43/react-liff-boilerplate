e93277052a0d473caa2a37f4d979327b8ea480f7
廖柏舜 <bensonliao@liaobaishunde-MacBook-Pro.local>
2019-02-22 18:13:19 +0800

    additional refactor on 3 pages component

--- src/pages/LIFFWindow.js
+++ src/pages/LIFFWindow.js
@@ -10,6 +10,7 @@ export default class LIFFWindow extends Component {
     this.setTextInputRef = this.setTextInputRef.bind(this)
     this.openLIFFWindow = this.openLIFFWindow.bind(this)
     this.openLIFFWindowExternal = this.openLIFFWindowExternal.bind(this)
+    this.closeWindow = liffHelper.closeWindow()
   }
 
   setTextInputRef = element => {
@@ -24,50 +25,36 @@ export default class LIFFWindow extends Component {
     liffHelper.openWindow(this.textInput.value, true)
   }
 
-  closeWindow() {
-    liffHelper.closeWindow()
-  }
-
   render() {
     return (
       <div className="page-content">
         <div className="col-lg-3" />
         <div className="col-lg-6">
           <div className="form-group">
-            <label htmlFor="userid">Url:</label>
-            <input 
-              ref={this.setTextInputRef} 
-              type="text" 
-              className="form-control" 
-              id="userid" 
-              defaultValue="https://www.google.com" 
-            />
+            <label htmlFor="userid">
+              Url:
+              <input
+                ref={this.setTextInputRef}
+                type="text"
+                className="form-control"
+                id="userid"
+                defaultValue="https://www.google.com"
+              />
+            </label>
           </div>
           <div className="form-group">
-            <button 
-              type="button" 
-              className="btn btn-default" 
-              onClick={this.openLIFFWindow}
-            >
-            Open Url in LINE Browser
+            <button type="button" className="btn btn-default" onClick={this.openLIFFWindow}>
+              Open Url in LINE Browser
             </button>
           </div>
           <div className="form-group">
-            <button 
-              type="button" 
-              className="btn btn-default" 
-              onClick={this.openLIFFWindowExternal}
-            >
-            Open Url in External Browser
+            <button type="button" className="btn btn-default" onClick={this.openLIFFWindowExternal}>
+              Open Url in External Browser
             </button>
           </div>
           <hr />
-          <button 
-            type="button" 
-            className="btn btn-default" 
-            onClick={this.closeWindow}
-          >
-          Close LIFF
+          <button type="button" className="btn btn-default" onClick={this.closeWindow}>
+            Close LIFF
           </button>
         </div>
         <div className="col-lg-3" />
--- src/pages/SendMessage.js
+++ src/pages/SendMessage.js
@@ -6,62 +6,63 @@ import liffHelper from '../utils/liffHelper'
 import messageHelper from '../utils/messagingApiHelper'
 
 const messageTypes = [
-  { 
-    key: 'text', 
-    label: 'Text', 
-    editable: true, 
-    value: 'give me brown' 
+  {
+    key: 'text',
+    label: 'Text',
+    editable: true,
+    value: 'give me brown'
   },
-  { 
-    key: 'image', 
-    label: 'Image', 
-    editable: true, 
-    value: 'https://developers.line.biz/media/messaging-api/using-line-url-scheme/camera-screen-3c7466e3.png' 
+  {
+    key: 'image',
+    label: 'Image',
+    editable: true,
+    value:
+      'https://developers.line.biz/media/messaging-api/using-line-url-scheme/camera-screen-3c7466e3.png'
   },
-  { 
-    key: 'video', 
-    label: 'Video', 
-    editable: false, 
-    value: 'https://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_5mb.mp4' 
+  {
+    key: 'video',
+    label: 'Video',
+    editable: false,
+    value: 'https://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_5mb.mp4'
   },
-  { 
-    key: 'audio', 
-    label: 'Audio', 
-    editable: false, 
-    value: 'https://cdn.online-convert.com/example-file/audio/m4a/example.m4a' 
+  {
+    key: 'audio',
+    label: 'Audio',
+    editable: false,
+    value: 'https://cdn.online-convert.com/example-file/audio/m4a/example.m4a'
   },
-  { 
-    key: 'location', 
-    label: 'Location', 
-    editable: false, 
-    value: 'Location' 
+  {
+    key: 'location',
+    label: 'Location',
+    editable: false,
+    value: 'Location'
   },
-  { 
-    key: 'button', 
-    label: 'Template - Button', 
-    editable: false, 
-    value: 'Button' 
+  {
+    key: 'button',
+    label: 'Template - Button',
+    editable: false,
+    value: 'Button'
   },
-  { 
-    key: 'confirm', 
-    label: 'Template - Confirm', 
-    editable: false, 
-    value: 'Confirm' 
+  {
+    key: 'confirm',
+    label: 'Template - Confirm',
+    editable: false,
+    value: 'Confirm'
   },
-  { 
-    key: 'carousel', 
-    label: 'Template - Carousel', 
-    editable: false, 
-    value: 'Carousel', 
-    disabled: true 
+  {
+    key: 'carousel',
+    label: 'Template - Carousel',
+    editable: false,
+    value: 'Carousel',
+    disabled: true
   },
-  { 
-    key: 'image-carousel', 
-    label: 'Template - Image carousel', 
-    editable: false, 
-    value: 'Image carousel', 
-    disabled: true 
-  },
+  {
+    key: 'image-carousel',
+    label: 'Template - Image carousel',
+    editable: false,
+    value: 'Image carousel',
+    disabled: true
+  }
 ]
 
 class SendMessage extends Component {
@@ -76,58 +77,73 @@ class SendMessage extends Component {
   }
 
   sendMessageToChat(messageKey) {
-    let value = this.textInput[messageKey].value
+    // let value = this.textInput[messageKey].value
+    const { [messageKey]: value } = this.textInput
+    const {
+      isGeolocationAvailable,
+      isGeolocationEnabled,
+      coords: { latitude, longitude }
+    } = this.props
+    let actions = [
+      {
+        type: 'uri',
+        label: 'Google',
+        uri: `https://google.com`
+      },
+      {
+        type: 'uri',
+        label: 'Facebook',
+        uri: `https://facebook.com`
+      }
+    ]
     let message
     switch (messageKey) {
       case 'text':
-        message = messageHelper.createTextMessage(value); break
+        message = messageHelper.createTextMessage(value)
+        break
       case 'image':
-        message = messageHelper.createImageMessage(value, value); break
+        message = messageHelper.createImageMessage(value, value)
+        break
       case 'video':
-        let video = 'https://www.sample-videos.com/img/Sample-png-image-500kb.png'
-        message = messageHelper.createVDOMessage(value, video); break
+        message = messageHelper.createVDOMessage(
+          value,
+          'https://www.sample-videos.com/img/Sample-png-image-500kb.png'
+        )
+        break
       case 'audio':
-        message = messageHelper.createAudioMessage(value, 3600); break
+        message = messageHelper.createAudioMessage(value, 3600)
+        break
       case 'button':
-        let actions = [
-          {
-            "type": "uri",
-            "label": "Google",
-            "uri": `https://google.com`
-          },
-          {
-            "type": "uri",
-            "label": "Facebook",
-            "uri": `https://facebook.com`
-          },
-        ]
-        message = messageHelper.createButtonMessageWithImage('Select Page', 'Send Buttton Demo', 'https://developers.line.biz/media/messaging-api/using-line-url-scheme/camera-screen-3c7466e3.png', actions)
+        message = messageHelper.createButtonMessageWithImage(
+          'Select Page',
+          'Send Buttton Demo',
+          'https://developers.line.biz/media/messaging-api/using-line-url-scheme/camera-screen-3c7466e3.png',
+          actions
+        )
         break
       case 'confirm':
-        let confirmActions = [
+        actions = [
           {
-            "type": "uri",
-            "label": "YES",
-            "uri": `https://google.com`
+            type: 'uri',
+            label: 'YES',
+            uri: `https://google.com`
           },
           {
-            "type": "uri",
-            "label": "NO",
-            "uri": `https://facebook.com`
+            type: 'uri',
+            label: 'NO',
+            uri: `https://facebook.com`
           }
         ]
-        message = messageHelper.createConfirmMessage('Send Message again', confirmActions)
+        message = messageHelper.createConfirmMessage('Send Message again', actions)
         break
       case 'location':
-        if (this.props.isGeolocationAvailable && this.props.isGeolocationEnabled) {
-          let lat = this.props.coords.latitude
-          let lng = this.props.coords.longitude
-          message = messageHelper.createLocationMessage(lat, lng)
+        if (isGeolocationAvailable && isGeolocationEnabled) {
+          message = messageHelper.createLocationMessage(latitude, longitude)
         } else {
           swal({
             type: 'error',
             title: 'Send Error',
-            text: 'Geolocation is not enabled',
+            text: 'Geolocation is not enabled'
           })
         }
         break
@@ -135,7 +151,8 @@ class SendMessage extends Component {
         message = messageHelper.createTextMessage('ABC')
     }
     if (message) {
-      liffHelper.sendMessages([message])
+      liffHelper
+        .sendMessages([message])
         .then(() => {
           swal({
             type: 'success',
@@ -144,41 +161,41 @@ class SendMessage extends Component {
             timer: 1000
           })
         })
-        .catch((err) => {
+        .catch(err => {
           swal({
             type: 'error',
             title: 'Send Error',
-            text: err.response.data.message,
+            text: err.response.data.message
           })
         })
     }
   }
 
   renderMessageTypeKey() {
-    return messageTypes.map((messageType) => (
+    return messageTypes.map(messageType => (
       <div className="form-group" key={messageType.key}>
-        <label 
-          htmlFor={`msg_${messageType.key}`} 
-          className="message-label">
-          {messageType.label}:
-        </label>
         <div className="input-group">
-          <input 
-            ref={this.setTextInputRef.bind(this, messageType.key)} 
-            id={`msg_${messageType.key}`} 
-            disabled={!messageType.editable} 
-            type="text" 
-            className="form-control" 
-            defaultValue={messageType.value} 
-          />
+          <label htmlFor={`msg_${messageType.key}`} className="message-label">
+            {messageType.label}
+            :
+            <input
+              ref={this.setTextInputRef.bind(this, messageType.key)}
+              id={`msg_${messageType.key}`}
+              disabled={!messageType.editable}
+              type="text"
+              className="form-control"
+              defaultValue={messageType.value}
+            />
+          </label>
           <span className="input-group-btn">
-          <button 
-            type="button" 
-            className="btn btn-default" 
-            disabled={messageType.disabled} 
-            onClick={this.sendMessageToChat.bind(this, `${messageType.key}`)} >
-            Send
-          </button>
+            <button
+              type="button"
+              className="btn btn-default"
+              disabled={messageType.disabled}
+              onClick={this.sendMessageToChat.bind(this, `${messageType.key}`)}
+            >
+              Send
+            </button>
           </span>
         </div>
       </div>
@@ -192,10 +209,13 @@ class SendMessage extends Component {
         <div className="col-lg-6">
           {this.renderMessageTypeKey()}
           <hr />
-          <button 
-            type="button" 
-            className="btn btn-default" 
-            onClick={() => { liffHelper.closeWindow() }}>
+          <button
+            type="button"
+            className="btn btn-default"
+            onClick={() => {
+              liffHelper.closeWindow()
+            }}
+          >
             Close LIFF
           </button>
         </div>
@@ -207,17 +227,23 @@ class SendMessage extends Component {
 
 export default geolocated({
   positionOptions: {
-    enableHighAccuracy: true,
+    enableHighAccuracy: true
   },
-  userDecisionTimeout: 5000,
+  userDecisionTimeout: 5000
 })(SendMessage)
 
-//For later implementation
+// For later implementation
 SendMessage.propTypes = {
   isGeolocationAvailable: PropTypes.bool,
   isGeolocationEnabled: PropTypes.bool,
   coords: PropTypes.shape({
     latitude: PropTypes.number,
-    longitude: PropTypes.number,
-  }),
+    longitude: PropTypes.number
+  })
+}
+
+SendMessage.defaultProps = {
+  isGeolocationAvailable: false,
+  isGeolocationEnabled: false,
+  coords: {}
 }
